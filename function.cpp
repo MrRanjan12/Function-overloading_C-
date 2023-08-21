93de27d2d639b68f6ded06bc1ce6c37330a22f58
Ranjan kumar <136085563+MrRanjan12@users.noreply.github.com>
2023-08-22 00:08:46 +0530

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+float simpleIntrest(int principle,int rate,int time){
+    return (principle*rate*time/100);
+}
+int factorial(int n){
+    if(n<=1){
+        return  1;
+    }
+    return n*factorial(n-1);
+}
+int main() {
+    cout<<"Enter the value of the following p,r&t "<<endl;
+    cout<<"Here y value is also for factorial "<<endl;
+    int x,y,z;
+    
+    cin>>x>>y>>z;
+   // cout<<"enter the value of p :"<endl;
+  //  cin>>n;
+    cout<<"simple interest :"<<simpleIntrest(x,y,z)<<endl;
+    cout<<"the factorial of " << y<< " is "<<factorial(y);
 
+    return 0;
+}
